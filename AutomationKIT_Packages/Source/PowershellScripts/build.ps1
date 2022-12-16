a49abc139a38a08ae61408a123bd99cca6165297
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2022-12-16 09:49:54 -0800

    Move reference path variables from relative path

--- AutomationKIT_Packages/Source/PowershellScripts/build.ps1
+++ AutomationKIT_Packages/Source/PowershellScripts/build.ps1
@@ -1,23 +1,19 @@
 param( [Parameter(Mandatory=$true)] $DropLocation )
 
-Push-Location
-
 Write-Host "Read files from $DropLocation"
-Set-Location "$($PSScriptRoot)\..\AutomationKITPackage_Main"
 
 $sourceManagedPackages = Get-ChildItem -path (($DropLocation) + "/drop/*_managed.zip") | Select-Object FullName
 Write-Host $sourceManagedPackages
 
-Copy-Item (($DropLocation) + "/drop/AutomationCoEMain*_managed.zip") -Destination PkgAssets
-$paths = Get-ChildItem -path "PkgAssets/AutomationCoEMain*.zip" | Select-Object FullName
+Copy-Item (($DropLocation) + "/drop/AutomationCoEMain*_managed.zip") -Destination "$($PSScriptRoot)\..\AutomationKITPackage_Main\PkgAssets"
+$paths = Get-ChildItem -path "$($PSScriptRoot)\..\AutomationKITPackage_Main\PkgAssets\AutomationCoEMain*.zip" | Select-Object FullName
 
 if ($paths.Count -ge 1) {
-    [xml]$xmlDoc = Get-Content -Path "PkgAssets/ImportConfig.xml"
+    [xml]$xmlDoc = Get-Content -Path "$($PSScriptRoot)\..\AutomationKITPackage_Main\PkgAssets\ImportConfig.xml"
     $newPakage = ([System.IO.Path]::GetFileName($paths[0].FullName).ToString())
     $xmlDoc.configdatastorage.solutions.configsolutionfile.solutionpackagefilename = "$newPakage"
-    $configPath = Get-ChildItem -path "PkgAssets/ImportConfig.xml" | Select-Object FullName
 
-    $configFile = $configPath[0].FullName
+    $configFile = "$($PSScriptRoot)\..\AutomationKITPackage_Main\PkgAssets\ImportConfig.xml"
     Write-Host $configFile
     $xmlDoc.save($configFile)
 } else {
@@ -27,12 +23,8 @@ if ($paths.Count -ge 1) {
 
 dotnet publish -f net472 -c Release
 
-$paths = Get-ChildItem -path "bin/Release/AutomationKit_Main*.zip" | Select-Object FullName
-
-Set-Location $($PSScriptRoot)
+$paths = Get-ChildItem -path "$($PSScriptRoot)\..\AutomationKITPackage_Main\bin\Release\AutomationKit_Main*.zip" | Select-Object FullName
 
 if ($paths.Count -ge 1) {
-    Copy-Item ($paths[0].FullName) -Destination "Microsoft_AutomationKIT_Main_Package.zip"
+    Copy-Item ($paths[0].FullName) -Destination "$($PSScriptRoot)\Microsoft_AutomationKIT_Main_Package.zip"
 }
-
-Pop-Location
