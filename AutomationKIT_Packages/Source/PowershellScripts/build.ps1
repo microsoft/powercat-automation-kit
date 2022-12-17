f66f3d79306a90929d5ff1f1867c1f2dfe859bc8
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2022-12-16 16:31:54 -0800

    Refactor update

--- AutomationKIT_Packages/Source/PowershellScripts/build.ps1
+++ AutomationKIT_Packages/Source/PowershellScripts/build.ps1
@@ -1,75 +1,80 @@
-param( [Parameter(Mandatory=$true)] $DropLocation )
+param ( 
+    [Parameter(Mandatory=$true)] $DropLocation
+)
+function Copy-Package {
+    param (
+        $DropLocation,
+        $type,
+        $name
+    )
+    $sourceManagedPackage = Get-ChildItem -path (($DropLocation) + "/drop/AutomationCoE$($name)*_managed.zip") | Select-Object FullName
+
+    Write-Host "Found following $type package"
+    Write-Host $sourceManagedPackage
+
+    if ( $null -ne $sourceManagedPackage ) {
+        Copy-Item $sourceManagedPackage.FullName -Destination "$($PSScriptRoot)\..\AutomationKITPackage_$($name)\PkgAssets"
+    }
+
+    $packageAsset = Get-ChildItem -path "$($PSScriptRoot)\..\AutomationKITPackage_$($name)\PkgAssets\AutomationCoE$($name)*.zip" | Select-Object FullName
+
+    if ( $null -ne $packageAsset) {
+        [xml]$xmlDoc = Get-Content -Path "$($PSScriptRoot)\..\AutomationKITPackage_$($name)\PkgAssets\ImportConfig.xml"
+        $newPackageName = ([System.IO.Path]::GetFileName($packageAsset.FullName).ToString())
+        $xmlDoc.configdatastorage.solutions.configsolutionfile.solutionpackagefilename = "$newPackageName"
+
+        $configFile = "$($PSScriptRoot)\..\AutomationKITPackage_$($name)\PkgAssets\ImportConfig.xml"
+        $xmlDoc.save($configFile)
+    } else {
+        Write-Error "Unable to find $type release package"
+        exit 1
+    }
+}
+
+function Build-Package {
+    param (
+        $name
+    )
+    Push-Location
+    Set-Location "$($PSScriptRoot)\..\AutomationKITPackage_$($name)"
+    dotnet publish -f net472 -c Release
+    Pop-Location
+}
+
+function Deploy-Package {
+    param (
+        $name
+    )
+
+    $packageDeployment = Get-ChildItem -path "$($PSScriptRoot)\..\AutomationKITPackage_$($name)\bin\Release\AutomationKit_$($name)*.zip" | Select-Object FullName
+
+    if ($null -ne $packageDeployment) {
+        Copy-Item ($packageDeployment.FullName) -Destination "$($PSScriptRoot)\Microsoft_AutomationKIT_$($name)_Package.zip"
+    } else {
+        Write-Error "Unable to find deployment package"
+        exit 1
+    }
+}
+
+function Invoke-PackageAndDeploy {
+    param (
+        $DropLocation,
+        $type,
+        $name
+    )
+
+    Copy-Package $DropLocation $type $Name
+
+    Build-Package $Name
+
+    Deploy-Package $Name
+}
+
+if ( $global:exitScript ) {
+	return
+}
 
 Write-Host "Read files from $DropLocation"
 
-$sourceManagedPackage = Get-ChildItem -path (($DropLocation) + "/drop/AutomationCoEMain*_managed.zip") | Select-Object FullName
-$sourceSatelliteManagedPackage = Get-ChildItem -path (($DropLocation) + "/drop/AutomationCoESatellite*_managed.zip") | Select-Object FullName
-
-Write-Host "Found following main package"
-Write-Host $sourceManagedPackage
-
-Write-Host "Found following satellite package"
-Write-Host $sourceSatelliteManagedPackage
-
-if ( $null -ne $sourceManagedPackage ) {
-    Copy-Item $sourceManagedPackage.FullName -Destination "$($PSScriptRoot)\..\AutomationKITPackage_Main\PkgAssets"
-}
-
-if ( $null -ne $sourceSatelliteManagedPackage ) {
-    Copy-Item $sourceSatelliteManagedPackage.FullName -Destination "$($PSScriptRoot)\..\AutomationKITPackage_Satellite\PkgAssets"
-}
-
-$packageAsset = Get-ChildItem -path "$($PSScriptRoot)\..\AutomationKITPackage_Main\PkgAssets\AutomationCoEMain*.zip" | Select-Object FullName
-$packageSatelliteAsset = Get-ChildItem -path "$($PSScriptRoot)\..\AutomationKITPackage_Satellite\PkgAssets\AutomationCoESatellite*.zip" | Select-Object FullName
-
-if ( $null -ne $packageAsset) {
-    [xml]$xmlDoc = Get-Content -Path "$($PSScriptRoot)\..\AutomationKITPackage_Main\PkgAssets\ImportConfig.xml"
-    $newPackageName = ([System.IO.Path]::GetFileName($packageAsset.FullName).ToString())
-    $xmlDoc.configdatastorage.solutions.configsolutionfile.solutionpackagefilename = "$newPackageName"
-
-    $configFile = "$($PSScriptRoot)\..\AutomationKITPackage_Main\PkgAssets\ImportConfig.xml"
-    $xmlDoc.save($configFile)
-} else {
-    Write-Error "Unable to find main release package"
-    exit 1
-}
-
-if ( $null -ne $packageSatelliteAsset) {
-    [xml]$xmlDoc = Get-Content -Path "$($PSScriptRoot)\..\AutomationKITPackage_Satellite\PkgAssets\ImportConfig.xml"
-    $newPackageName = ([System.IO.Path]::GetFileName($packageAsset.FullName).ToString())
-    $xmlDoc.configdatastorage.solutions.configsolutionfile.solutionpackagefilename = "$newPackageName"
-
-    $configFile = "$($PSScriptRoot)\..\AutomationKITPackage_Satellite\PkgAssets\ImportConfig.xml"
-    $xmlDoc.save($configFile)
-} else {
-    Write-Error "Unable to find Satellite release package"
-    exit 1
-}
-
-Push-Location
-Set-Location "$($PSScriptRoot)\..\AutomationKITPackage_Main"
-dotnet publish -f net472 -c Release
-Pop-Location
-
-Push-Location
-Set-Location "$($PSScriptRoot)\..\AutomationKITPackage_Satellite"
-dotnet publish -f net472 -c Release
-Pop-Location
-
-$packageDeployment = Get-ChildItem -path "$($PSScriptRoot)\..\AutomationKITPackage_Main\bin\Release\AutomationKit_Main*.zip" | Select-Object FullName
-
-if ($null -ne $packageDeployment) {
-    Copy-Item ($packageDeployment.FullName) -Destination "$($PSScriptRoot)\Microsoft_AutomationKIT_Main_Package.zip"
-} else {
-    Write-Error "Unable to find deployment package"
-    exit 1
-}
-
-$packageSatelliteDeployment = Get-ChildItem -path "$($PSScriptRoot)\..\AutomationKITPackage_Satellite\bin\Release\AutomationKit_Satellite*.zip" | Select-Object FullName
-
-if ($null -ne $packageSatelliteDeployment) {
-    Copy-Item ($packageSatelliteDeployment.FullName) -Destination "$($PSScriptRoot)\Microsoft_AutomationKIT_Satellite_Package.zip"
-} else {
-    Write-Error "Unable to find satellite deployment package"
-    exit 1
-}
+Invoke-PackageAndDeploy $DropLocation "main" "Main" 
+Invoke-PackageAndDeploy $DropLocation "satellite" "Satellite" 
