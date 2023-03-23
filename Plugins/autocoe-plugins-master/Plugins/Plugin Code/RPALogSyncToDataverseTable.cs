269ac55c1c6e354f07bd27e4bccd216439a3e944
KOTESWARA RAO MARAM <107620140+KoteshMCGit@users.noreply.github.com>
2023-03-23 15:48:32 +0530

    Plug-in code refactoring is done
    Code refactor is done for below items
    1. Removed ILMerge dependency
    2. Removed Newtonsoft.Json dependency
    3. Minor formatting changes for plug-in classes.
    Now all external dependencies are removed and building a single dll can be registered.

--- Plugins/autocoe-plugins-master/Plugins/Plugin Code/RPALogSyncToDataverseTable.cs
+++ Plugins/autocoe-plugins-master/Plugins/Plugin Code/RPALogSyncToDataverseTable.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Linq;
-using AutoCoE.Extensibility.Plugins.DataModel;
-using AutoCoE.Extensibility.Plugins.PluginHelper;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Query;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-
 namespace AutoCoE.Extensibility.Plugins
 {
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Text.Json;
+    using AutoCoE.Extensibility.Plugins.DataModel;
+    using AutoCoE.Extensibility.Plugins.PluginHelper;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Query;
+
     /// <summary>
     /// RPALogSyncToDataverseTable Plugin.
     /// </summary>    
@@ -47,6 +47,7 @@ namespace AutoCoE.Extensibility.Plugins
             {
                 throw new InvalidPluginExecutionException(nameof(localContext));
             }
+
             // Obtain the tracing service
             ITracingService tracingService = localContext.TracingService;
 
@@ -66,12 +67,16 @@ namespace AutoCoE.Extensibility.Plugins
                     try
                     {
                         if (!context.InputParameters.Contains("FlowSessionLogId"))
+                        {
                             throw new InvalidPluginExecutionException($"We couldn't find a valid Flow Session Log Id in the context.InputParameters Collection.");
+                        }
 
                         autocoe_FlowSessionLog flowSessionLog = currentUserService.Retrieve(autocoe_FlowSessionLog.EntityLogicalName, Guid.Parse(context.InputParameters["FlowSessionLogId"].ToString()), new ColumnSet(true)).ToEntity<autocoe_FlowSessionLog>();
 
                         if (flowSessionLog == null)
+                        {
                             throw new InvalidPluginExecutionException($"We couldn't find a valid Flow Session Log for Id: {context.InputParameters["FlowSessionLogId"]}.");
+                        }
 
                         tracingService.Trace($"Automation CoE - RPALogSyncToDataverseTable - Flow Session Log Id: {flowSessionLog.Id} ");
 
@@ -81,19 +86,23 @@ namespace AutoCoE.Extensibility.Plugins
 
                             if (flowSessionLog.autocoe_DesktopFlowStatus.Equals("Failed"))
                             {
+                                // Sample content "{\"actions\":[{\"name\":\"Display select file dialog\",\"systemActionName\":\"SelectFileDialog\",\"systemModuleName\":\"Display\",\"localizedModuleName\":\"Message boxes\",\"startTime\":\"2023-02-20T10:44:35.7885613Z\",\"endTime\":\"2023-02-20T11:01:19.9636296Z\",\"index\":2,\"functionName\":\"main\",\"insideErrorHandling\":false,\"status\":\"Succeeded\",\"inputs\":{\"allowMultiple\":{\"localizedName\":\"Allow multiple selection\",\"value\":\"False\",\"variableName\":\"\"},\"title\":{\"localizedName\":\"Dialog title\",\"value\":\"Select the excel file to extract table from...\",\"variableName\":\"\"},\"fileFilter\":{\"localizedName\":\"File filter\",\"value\":\"*.xls*\",\"variableName\":\"\"},\"isTopMost\":{\"localizedName\":\"Keep file selection dialog always on top\",\"value\":\"True\",\"variableName\":\"\"},\"checkIfFileExists\":{\"localizedName\":\"Check if file exists\",\"value\":\"True\",\"variableName\":\"\"}},\"outputs\":{\"selectedFile\":{\"localizedName\":\"Selected file\",\"value\":\"\",\"variableName\":\"SelectedFile\"},\"buttonPressed\":{\"localizedName\":\"Button pressed\",\"value\":\"Cancel\",\"variableName\":\"ButtonPressed\"}}},{\"name\":\"If\",\"systemActionName\":\"If\",\"systemModuleName\":\"Conditionals\",\"localizedModuleName\":\"Conditionals\",\"startTime\":\"2023-02-20T11:01:20.0370639Z\",\"endTime\":\"2023-02-20T11:01:20.0478908Z\",\"index\":4,\"functionName\":\"main\",\"insideErrorHandling\":false,\"status\":\"Succeeded\",\"inputs\":{\"expression\":{\"localizedName\":\"Condition\",\"value\":\"False\",\"variableName\":\"\"}},\"outputs\":{}}],\"childFlowsLogStatus\":\"Default\"}";
                                 string flowContext = PluginUtility.ExtractAdditionalContext(currentUserService, Guid.Parse(flowSessionLog.autocoe_FlowSessionId));
                                 PADActionLog padActions = PluginUtility.Deserialize<PADActionLog>(flowContext);
 
                                 // Max log entry size seems to be 10k rows, so no need to add so many rows if the action that threw the error is not included anyhow
                                 if (padActions.actions.Where(e => e.status == "Failed").Count() > 0)
                                 {
-                                    var actionList = ((JArray)JObject
-                                        .Parse(flowContext)["actions"])
-                                        .Where(a => a["status"].ToString() == "Failed")
-                                        .FirstOrDefault();
+                                    // var actionList = ((JArray)JObject
+                                    //    .Parse(flowContext)["actions"])
+                                    //    .Where(a => a["status"].ToString() == "Failed")
+                                    //    .FirstOrDefault();
 
-                                    string desinput = JsonConvert.DeserializeObject(actionList["inputs"].ToString(Newtonsoft.Json.Formatting.Indented)).ToString();
-                                    string desoutput = JsonConvert.DeserializeObject(actionList["outputs"].ToString(Newtonsoft.Json.Formatting.Indented)).ToString();
+                                    // string desinput = JsonConvert.DeserializeObject(actionList["inputs"].ToString(Newtonsoft.Json.Formatting.Indented)).ToString();
+                                    //// string desoutput = JsonConvert.DeserializeObject(actionList["outputs"].ToString(Newtonsoft.Json.Formatting.Indented)).ToString();
+
+                                    string desinput = this.ParseInputandOutput(flowContext, "inputs");
+                                    string desoutput = this.ParseInputandOutput(flowContext, "outputs");
 
                                     var failingAction = padActions.actions.Where(e => e.status == "Failed").First();
                                     flowSessionLog.autocoe_FailingDesktopFlowModule = failingAction.localizedModuleName;
@@ -102,16 +111,16 @@ namespace AutoCoE.Extensibility.Plugins
                                     flowSessionLog.autocoe_DesktopFlowActionErrorCode = failingAction.error.code;
                                     flowSessionLog.autocoe_DesktopFlowActionErrorMessage = failingAction.error.message;
                                     flowSessionLog.autocoe_ActionInput = desinput;
-                                    flowSessionLog.autocoe_ActionOutput= desoutput;
+                                    flowSessionLog.autocoe_ActionOutput = desoutput;
 
                                     currentUserService.Update(flowSessionLog);
                                 }
-                                else 
+                                else
                                 {
                                     if (padActions.actions.Count > 10000)
                                     {
                                         flowSessionLog.autocoe_DesktopFlowActionErrorMessage = "!-->>> The desktop flow error occurred after reaching the current max action log row size (10k) supported by the PAD log mechanism. Therefore, no action error details are included in the logs. <<<--!";
-                                    } 
+                                    }
 
                                     currentUserService.Update(flowSessionLog);
                                 }
@@ -131,12 +140,35 @@ namespace AutoCoE.Extensibility.Plugins
                     tracingService.Trace("Automation CoE - RPALogSyncToDataverseTable plug-in is not associated with the expected message or is not registered for the main operation.");
                 }
             }
-            // Only throw an InvalidPluginExecutionException. Please Refer https://go.microsoft.com/fwlink/?linkid=2153829.
             catch (Exception ex)
             {
                 tracingService?.Trace("An error occurred executing Plugin AutoCoE.Extensibility.Plugins.RPALogSyncToDataverseTable : {0}", ex.ToString());
                 throw new InvalidPluginExecutionException("An error occurred executing Plugin AutoCoE.Extensibility.Plugins.RPALogSyncToDataverseTable .", ex);
             }
+        }
+
+        /// <summary>
+        /// Parses 'flowContextDetails' and return 'RawText' of 'input' and 'output' parameters.
+        /// </summary>
+        /// <param name="flowContextDetails">Flow Context Details</param>
+        /// <param name="propertyName">Property Name</param>
+        /// <returns>Raw Text of Parameter</returns>
+        private string ParseInputandOutput(string flowContextDetails, string propertyName)
+        {
+            StringBuilder sbrRawText = new StringBuilder();
+            JsonDocument doc = JsonDocument.Parse(flowContextDetails);
+            JsonElement actions = doc.RootElement.GetProperty("actions");
+
+            foreach (JsonElement action in actions.EnumerateArray())
+            {
+                if (action.GetProperty("status").GetString() == "Failed")
+                {
+                    JsonElement propertyNodes = action.GetProperty(propertyName);
+                    sbrRawText.AppendLine(propertyNodes.ToString());
+                }
+            }
+
+            return sbrRawText.ToString();
         }
     }
 }
