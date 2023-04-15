c192589e5dd795ab5ff66ac2f8d8b9304d55ddfb
Pujarini Mohapatra <biswapuja0902@gmail.com>
2023-04-15 23:28:20 +0530

    Update flowsessionview.sql

--- AutomationKit_Flow_BYODL/Control Center/Flow Monitoring/Power BI/Scripts/flowsessionview.sql
+++ AutomationKit_Flow_BYODL/Control Center/Flow Monitoring/Power BI/Scripts/flowsessionview.sql
@@ -1,10 +1,15 @@
+-- Create a new schema named "reports"
 CREATE SCHEMA reports
 
+-- Drop the view if it already exists
 DROP VIEW IF EXISTS reports.flowsessionview;
 GO
 
-CREATE view reports.flowsessionView as 
-SELECT 
+-- Create a new view named "flowsessionView" within the "reports" schema
+CREATE VIEW reports.flowsessionView AS 
+
+-- Select data from the "flowsession" table and join it with data from the "flowmachinegroup", "flowmachine", and "StatusMetadata" tables
+SELECT top 100
   [flowsessionid], 
   [$flowsession].[createdon], 
   [$flowsession].[statuscode], 
@@ -19,18 +24,28 @@ SELECT
   [regardingobjectidname], 
   [machineidname], 
   [machinegroupidname], 
-CONVERT(date, [completedon]) as [reportingdate], -- convert completedon to date format
-  CASE WHEN [context] LIKE '%"attendedMode":"Attended"%' THEN 'Attended' 
-       WHEN [context] LIKE '%"attendedMode":"Unattended"%' THEN 'Unattended' 
-       ELSE 'Local' 
+
+  -- Convert "completedon" to a date format and rename the column as "reportingdate"
+  CONVERT(date, [completedon]) as [reportingdate], 
+
+  -- Derive the run mode of the flow session based on the "context" column, and rename the column as "runmode"
+  CASE 
+    WHEN [context] LIKE '%"attendedMode":"Attended"%' THEN 'Attended' 
+    WHEN [context] LIKE '%"attendedMode":"Unattended"%' THEN 'Unattended' 
+    ELSE 'Local' 
   END AS [runmode], 
   
-  CASE WHEN [$flowmachine].hostingtype = 1 AND [$flowmachine].[hostingtype] IS NOT NULL THEN NULL 
-       ELSE [$flowmachine].[name] 
+  -- Conditionally rename the "name" columns from the "flowmachine" and "flowmachinegroup" tables
+  CASE 
+    WHEN [$flowmachine].hostingtype = 1 AND [$flowmachine].[hostingtype] IS NOT NULL THEN NULL 
+    ELSE [$flowmachine].[name] 
   END AS [reportingmachinename], 
-  CASE WHEN [$flowmachinegroup].[flowgrouptype] = 545940002 AND [$flowmachinegroup].[flowgrouptype] IS NOT NULL THEN NULL 
-       ELSE [$flowmachinegroup].[name] 
+  CASE 
+    WHEN [$flowmachinegroup].[flowgrouptype] = 545940002 AND [$flowmachinegroup].[flowgrouptype] IS NOT NULL THEN NULL 
+    ELSE [$flowmachinegroup].[name] 
   END AS [reportingmachinegroupname], 
+
+  -- Retrieve the localized label for the "statuscode" column from the "StatusMetadata" table, and rename the column as "statuscodename"
   [$statusmetadata].LocalizedLabel AS [statuscodename] 
 FROM 
   [dbo].[flowsession] AS [$flowsession] 
@@ -44,5 +59,10 @@ FROM
     [$flowsession].statuscode = [$statusmetadata].[Status] 
     AND [$statusmetadata].EntityName = 'flowsession'
   )
-   WHERE 
-[$flowsession].[statuscode] <> 3  AND [$flowsession].[statuscode] <> 2 
+   
+-- Filter the rows based on the statuscode
+WHERE 
+  [$flowsession].[statuscode] <> 3  -- exclude flowsession with status "Canceled"
+  AND [$flowsession].[statuscode] <> 2  -- exclude flowsession with status "Active"
+
+-- End of script
