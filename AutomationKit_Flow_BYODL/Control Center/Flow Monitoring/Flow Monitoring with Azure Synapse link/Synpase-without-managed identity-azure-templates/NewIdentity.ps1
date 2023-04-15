2714323543836a34de523eda849c5b27c302cb19
Pujarini Mohapatra <biswapuja0902@gmail.com>
2023-04-16 00:19:24 +0530

    managedidentity

---
+++ AutomationKit_Flow_BYODL/Control Center/Flow Monitoring/Flow Monitoring with Azure Synapse link/Synpase-without-managed identity-azure-templates/NewIdentity.ps1
@@ -0,0 +1,26 @@
+﻿# Load thescript
+. "$PSScriptRoot\..\Common\EnvironmentEnterprisePolicyOperations.ps1"
+
+function NewIdentity 
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$environmentId,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$policyArmId,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("tip1", "tip2", "prod")]
+        [String]$endpoint
+
+    )
+    
+    if (![bool]$endpoint) {
+        $endpoint = "prod"
+    }
+    LinkPolicyToEnv -policyType identity -environmentId $environmentId -policyArmId $policyArmId -endpoint $endpoint 
+}
+NewIdentity
