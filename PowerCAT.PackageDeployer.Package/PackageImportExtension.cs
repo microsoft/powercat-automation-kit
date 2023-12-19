3847ef3db640b7435643c983cd968f044a931d9c
Rajeev Pentyala <rajeevpe@microsoft.com>
2023-12-19 12:13:56 +0530

    Added Util classes

--- PowerCAT.PackageDeployer.Package/PackageImportExtension.cs
+++ PowerCAT.PackageDeployer.Package/PackageImportExtension.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.Composition;
+using System.IO;
+using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Tooling.PackageDeployment.CrmPackageExtentionBase;
 
 namespace PowerCAT.PackageDeployer.Package
@@ -21,19 +23,22 @@ namespace PowerCAT.PackageDeployer.Package
         /// Name of the Import Package to Use
         /// </summary>
         /// <param name="plural">if true, return plural version</param>
-        public override string GetNameOfImport(bool plural) => "PowerCAT.PackageDeployer.Package";
+        public override string GetNameOfImport(bool plural) => "PowerCATPackage";
 
         /// <summary>
         /// Long name of the Import Package.
         /// </summary>
-        public override string GetLongNameOfImport => "PowerCAT.PackageDeployer.Package";
+        public override string GetLongNameOfImport => "PowerCATPackage";
 
         /// <summary>
         /// Description of the package, used in the package selection UI
         /// </summary>
-        public override string GetImportPackageDescriptionText => "PowerCAT.PackageDeployer.Package";
+        public override string GetImportPackageDescriptionText => "PowerCATPackage";
 
         #endregion
+
+        public string FilePath { get; set; }
+        public string ProjectName { get; set; }
 
         /// <summary>
         /// Called to Initialize any functions in the Custom Extension.
@@ -41,6 +46,60 @@ namespace PowerCAT.PackageDeployer.Package
         /// <see cref="ImportExtension.InitializeCustomExtension"/>
         public override void InitializeCustomExtension()
         {
+            // Validate the state of the runtime settings object.  
+            if (RuntimeSettings != null)
+            {
+                PackageLog.Log(string.Format("Runtime Settings populated.  Count = {0}", RuntimeSettings.Count));
+                foreach (var setting in RuntimeSettings)
+                {
+                    PackageLog.Log(string.Format("Key={0} | Value={1}", setting.Key, setting.Value.ToString()));
+                }
+
+                // Check to file path provided.  
+                if (RuntimeSettings.ContainsKey("filepath"))
+                {
+                    FilePath = RuntimeSettings["filepath"].ToString();
+                }
+
+                if (RuntimeSettings.ContainsKey("projectname"))
+                {
+                    ProjectName = RuntimeSettings["projectname"].ToString();
+                }
+            }
+            else
+            {
+                PackageLog.Log("Runtime Settings not populated");
+            }
+
+            if (string.IsNullOrEmpty(FilePath) || string.IsNullOrEmpty(ProjectName))
+            {
+                PackageLog.Log("Missing required parameters.  Skipping PreDeploymentSettings and PostDeploymentSettings import.");
+                return;
+            }
+
+            Console.WriteLine($"Project Name : {ProjectName}");
+            Console.WriteLine($"File Path : {FilePath}");
+
+            var preDeploymentSettings = JsonUtility.ReadPreDeploymentSettings(FilePath, ProjectName);
+
+            if (preDeploymentSettings != null)
+            {
+                // Process the PreDeploymentSettings dictionary
+                Console.WriteLine($"PreDeploymentSettings for {ProjectName}:");
+
+                var organizationSettingsUpdater = new OrganizationSettingsUpdater(CrmSvc);
+
+                foreach (var setting in preDeploymentSettings)
+                {
+                    Console.WriteLine($"Updating Key : {setting.Key} Value: {setting.Value}");
+                    // Call UpdateOrganizationSettings with setting.Key and setting.Value
+                    organizationSettingsUpdater.UpdateOrganizationSettings(setting.Key, setting.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Project with name '{ProjectName}' or PreDeploymentSettings not found.");
+            }
         }
 
         /// <summary>
@@ -64,6 +123,9 @@ namespace PowerCAT.PackageDeployer.Package
         /// <param name="publishWorkflowsAndActivatePlugins">If set to true, attempts to auto publish workflows and activities as part of solution deployment</param>
         public override void PreSolutionImport(string solutionName, bool solutionOverwriteUnmanagedCustomizations, bool solutionPublishWorkflowsAndActivatePlugins, out bool overwriteUnmanagedCustomizations, out bool publishWorkflowsAndActivatePlugins)
         {
+            Console.WriteLine($"PreSolutionImport : {solutionName}");
+            Console.WriteLine($"Project Name : {ProjectName}");
+            Console.WriteLine($"File Path : {FilePath}");
             base.PreSolutionImport(solutionName, solutionOverwriteUnmanagedCustomizations, solutionPublishWorkflowsAndActivatePlugins, out overwriteUnmanagedCustomizations, out publishWorkflowsAndActivatePlugins);
         }
 
@@ -89,6 +151,9 @@ namespace PowerCAT.PackageDeployer.Package
         /// <returns></returns>
         public override bool AfterPrimaryImport()
         {
+            Console.WriteLine("AfterPrimaryImport");
+            Console.WriteLine($"Project Name : {ProjectName}");
+            Console.WriteLine($"File Path : {FilePath}");
             return true;
         }
     }
---
+++ PowerCAT.PackageDeployer.Package/Utils/JsonUtility.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+/// <summary>
+/// Utility class for working with JSON data.
+/// </summary>
+public class JsonUtility
+{
+    /// <summary>
+    /// Reads the PreDeploymentSettings from the specified JSON file for a given project name.
+    /// </summary>
+    /// <param name="filePath">The path to the JSON file.</param>
+    /// <param name="projectName">The name of the project to retrieve PreDeploymentSettings for.</param>
+    /// <returns>A dictionary containing the PreDeploymentSettings, or null if the project or settings are not found.</returns>
+    public static Dictionary<string, string> ReadPreDeploymentSettings(string filePath, string projectName)
+    {
+        try
+        {
+            // Read the entire file content
+            string jsonContent = File.ReadAllText(filePath);
+
+            // Parse the JSON data
+            JsonDocument jsonDocument = JsonDocument.Parse(jsonContent);
+
+            // Find the Project node with the specified name
+            JsonElement projectNode = FindProjectNode(jsonDocument.RootElement.GetProperty("Project"), projectName);
+
+            if (projectNode.ValueKind != JsonValueKind.Null)
+            {
+                // Extract PreDeploymentSettings into a dictionary
+                return ExtractSettings(projectNode, "PreDeploymentSettings");
+            }
+
+            return null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading or processing JSON data: {ex.Message}");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Reads the PostDeploymentSettings from the specified JSON file for a given project name.
+    /// </summary>
+    /// <param name="filePath">The path to the JSON file.</param>
+    /// <param name="projectName">The name of the project to retrieve PostDeploymentSettings for.</param>
+    /// <returns>A dictionary containing the PostDeploymentSettings, or null if the project or settings are not found.</returns>
+    public static Dictionary<string, string> ReadPostDeploymentSettings(string filePath, string projectName)
+    {
+        try
+        {
+            // Read the entire file content
+            string jsonContent = File.ReadAllText(filePath);
+
+            // Parse the JSON data
+            JsonDocument jsonDocument = JsonDocument.Parse(jsonContent);
+
+            // Find the Project node with the specified name
+            JsonElement projectNode = FindProjectNode(jsonDocument.RootElement.GetProperty("Project"), projectName);
+
+            if (projectNode.ValueKind != JsonValueKind.Null)
+            {
+                // Extract PostDeploymentSettings into a dictionary
+                return ExtractSettings(projectNode, "PostDeploymentSettings");
+            }
+
+            return null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading or processing JSON data: {ex.Message}");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Finds the Project node with the specified name.
+    /// </summary>
+    /// <param name="projectsNode">The JSON node containing the list of projects.</param>
+    /// <param name="projectName">The name of the project to find.</param>
+    /// <returns>The Project node if found, or Null if not found.</returns>
+    private static JsonElement FindProjectNode(JsonElement projectsNode, string projectName)
+    {
+        foreach (JsonElement projectNode in projectsNode.EnumerateArray())
+        {
+            if (projectNode.GetProperty("name").GetString() == projectName)
+            {
+                return projectNode;
+            }
+        }
+
+        // Return Null if not found
+        return default;
+    }
+
+    /// <summary>
+    /// Extracts the specified settings type from the Project node into a dictionary.
+    /// </summary>
+    /// <param name="projectNode">The Project node containing settings.</param>
+    /// <param name="settingsType">The type of settings to extract (e.g., PreDeploymentSettings).</param>
+    /// <returns>A dictionary containing the extracted settings.</returns>
+    private static Dictionary<string, string> ExtractSettings(JsonElement projectNode, string settingsType)
+    {
+        var settings = new Dictionary<string, string>();
+
+        // Check if the specified settings type exists
+        if (projectNode.TryGetProperty(settingsType, out JsonElement settingsNode))
+        {
+            foreach (JsonElement settingNode in settingsNode.EnumerateArray())
+            {
+                string settingName = settingNode.GetProperty("settingname").GetString();
+                string settingValue = settingNode.GetProperty("value").GetString();
+                settings.Add(settingName, settingValue);
+            }
+        }
+
+        return settings;
+    }
+}
